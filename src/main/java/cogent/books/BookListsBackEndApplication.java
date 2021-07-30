20bed53aa767eea1ba0e29976b9a84fd0563fee8
jacklong1996 <88209304+jacklong1996@users.noreply.github.com>
2021-07-30 14:17:03 -0500

    Added Springboot project
    Added Springboot project

---
+++ src/main/java/cogent/books/BookListsBackEndApplication.java
@@ -0,0 +1,13 @@
+package cogent.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookListsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookListsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cogent/books/BookListsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cogent.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookListsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
